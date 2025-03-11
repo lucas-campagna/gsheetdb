6e684a3281e560f5f98aa1a9a28f2a539ed50831
Your Name <you@example.com>
2025-03-11 09:47:20 -0300

    fix module

--- src/index.js
+++ src/index.js
@@ -1,49 +1,44 @@
 class Sheet {
-    constructor(config){
-        if (!config.deploymentId){
-            throw 'No DeploymentId provided';
-        }
-        const { token, username, password, deploymentId } = config;
-        const auth = token
-            ? { token }
-            : (
-                username && password
-                ? { username, password }
-                : null
-            );
-        if (!auth) {
-            throw `You tried to auth with username and password but you do not provided either username or passoword`;
-        }
-        this.fetch = body =>
-            fetch(
-                `https://script.google.com/macros/s/${deploymentId}/exec`,
-                {
-                    method: 'POST',
-                    body: JSON.stringify({ ...body, ...auth })
-                }
-            )
-            .then(p => p.json())
-            .then(({success, message}) => {
-                if (!success) {
-                    throw message;
-                }
-                return message
-            });
-    };
-    tables() {
-        return this.fetch({ action: 'tables' });
+  constructor(config) {
+    if (!config.deploymentId) {
+      throw "No DeploymentId provided";
     }
-    get(table, query) {
-        return this.fetch({ action: 'get', table, query });
-    };
-    set(table, items) {
-        return this.fetch({ action: 'set', table, items });
-    };
-    rm(table, ids) {
-        return this.fetch({ action: 'rm', table, ids });
-    };
-    new(table, header) {
-        return this.fetch({ action: 'new', table, header });
+    const { token, username, password, deploymentId } = config;
+    const auth = token
+      ? { token }
+      : username && password
+      ? { username, password }
+      : null;
+    if (!auth) {
+      throw `You tried to auth with username and password but you do not provided either username or passoword`;
     }
-};
-export default Sheet;
+    this.fetch = (body) =>
+      fetch(`https://script.google.com/macros/s/${deploymentId}/exec`, {
+        method: "POST",
+        body: JSON.stringify({ ...body, ...auth }),
+      })
+        .then((p) => p.json())
+        .then(({ success, message }) => {
+          if (!success) {
+            throw message;
+          }
+          return message;
+        });
+  }
+  tables() {
+    return this.fetch({ action: "tables" });
+  }
+  get(table, query) {
+    return this.fetch({ action: "get", table, query });
+  }
+  set(table, items) {
+    return this.fetch({ action: "set", table, items });
+  }
+  rm(table, ids) {
+    return this.fetch({ action: "rm", table, ids });
+  }
+  new(table, header) {
+    return this.fetch({ action: "new", table, header });
+  }
+}
+export { Sheet };
